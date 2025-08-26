13d728235de144c3fb507a047fd4c71d2c0e3172
KN <kn@kn.com>
2025-08-26 09:24:34 +0800

    ASD

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello KASDASDASDN " + name + "!!"
+    return " " + name + "!!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch() 
